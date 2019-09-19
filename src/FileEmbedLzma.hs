44525369a551f69d5014a1b29a8916c84bd393b8
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-09-19 22:53:30 +0300

    Updates 2019-09-19

--- src/FileEmbedLzma.hs
+++ src/FileEmbedLzma.hs
@@ -44,23 +44,23 @@ import System.IO.Unsafe                 (unsafePerformIO)
 
 import qualified Codec.Compression.Lzma  as LZMA
 import qualified Data.ByteString         as BS
-import qualified Data.ByteString.Lazy    as BSL
+import qualified Data.ByteString.Lazy    as LBS
 import qualified Data.ByteString.Unsafe  as BS.Unsafe
 import qualified Data.Text               as T
-import qualified Data.Text.Lazy          as TL
-import qualified Data.Text.Lazy.Encoding as TLE
+import qualified Data.Text.Lazy          as LT
+import qualified Data.Text.Lazy.Encoding as LTE
 
 import Instances.TH.Lift ()
 
-listRecursiveDirectoryFiles :: FilePath -> IO [(FilePath, BSL.ByteString)]
+listRecursiveDirectoryFiles :: FilePath -> IO [(FilePath, LBS.ByteString)]
 listRecursiveDirectoryFiles = listDirectoryFilesF listRecursiveDirectoryFiles
 
-listDirectoryFiles  :: FilePath -> IO [(FilePath, BSL.ByteString)]
+listDirectoryFiles  :: FilePath -> IO [(FilePath, LBS.ByteString)]
 listDirectoryFiles = listDirectoryFilesF (\_ -> return [])
 
 listDirectoryFilesF
-    :: (FilePath -> IO [(FilePath, BSL.ByteString)]) -- ^ what to do with a sub-directory
-    -> FilePath -> IO [(FilePath, BSL.ByteString)]
+    :: (FilePath -> IO [(FilePath, LBS.ByteString)]) -- ^ what to do with a sub-directory
+    -> FilePath -> IO [(FilePath, LBS.ByteString)]
 listDirectoryFilesF go topdir = do
     names <- getDirectoryContents topdir
     let properNames = filter (`notElem` [".", ".."]) names
@@ -70,25 +70,25 @@ listDirectoryFilesF go topdir = do
         if isDirectory
         then go path
         else do
-            contents <- BSL.readFile path
+            contents <- LBS.readFile path
             return [(path, contents)]
     return (concat paths)
 
 makeAllRelative :: FilePath -> [(FilePath, a)] -> [(FilePath, a)]
 makeAllRelative topdir = map (first (("/" ++) . makeRelative topdir))
 
--- | Makes lazy 'BSL.ByteString' expression.
+-- | Makes lazy 'LBS.ByteString' expression.
 -- Embedded value is compressed with LZMA.
-lazyBytestringE :: BSL.ByteString -> Q Exp
+lazyBytestringE :: LBS.ByteString -> Q Exp
 lazyBytestringE lbs =
     [| LZMA.decompress
-    $ BSL.fromStrict
+    $ LBS.fromStrict
     $ unsafePerformIO
     $ BS.Unsafe.unsafePackAddressLen $l $s
-    :: BSL.ByteString
+    :: LBS.ByteString
     |]
   where
-    bs = BSL.toStrict $ LZMA.compressWith params lbs
+    bs = LBS.toStrict $ LZMA.compressWith params lbs
     s = litE $ stringPrimL $ BS.unpack bs
     l = litE $ integerL $ fromIntegral $ BS.length bs
 
@@ -101,20 +101,20 @@ lazyBytestringE lbs =
 
 makeEmbeddedEntry :: Name -> (FilePath, (Int64, Int64)) -> Q Exp
 makeEmbeddedEntry name (path, (off, len)) =
-    [| (path, BSL.toStrict $ BSL.take len $ BSL.drop off $(varE name)) |]
+    [| (path, LBS.toStrict $ LBS.take len $ LBS.drop off $(varE name)) |]
 
-concatEntries :: Traversable t => t BSL.ByteString -> (BSL.ByteString, t (Int64, Int64))
-concatEntries xs = (bslEndo BSL.empty, ys)
+concatEntries :: Traversable t => t LBS.ByteString -> (LBS.ByteString, t (Int64, Int64))
+concatEntries xs = (bslEndo LBS.empty, ys)
   where
     (ys, (_, bslEndo)) = runState (traverse (state . single) xs) (0, id)
 
     single
-        :: BSL.ByteString                             -- file bytestring
-        -> (Int64, BSL.ByteString -> BSL.ByteString)  -- current offset, buffer so far
-        -> ((Int64, Int64), (Int64, BSL.ByteString -> BSL.ByteString))
-    single bsl (off, endo) = ((off, l), (off + l, endo . BSL.append bsl))
+        :: LBS.ByteString                             -- file bytestring
+        -> (Int64, LBS.ByteString -> LBS.ByteString)  -- current offset, buffer so far
+        -> ((Int64, Int64), (Int64, LBS.ByteString -> LBS.ByteString))
+    single bsl (off, endo) = ((off, l), (off + l, endo . LBS.append bsl))
       where
-        l = fromIntegral $ BSL.length bsl
+        l = fromIntegral $ LBS.length bsl
 
 -------------------------------------------------------------------------------
 -- Directories
@@ -128,7 +128,7 @@ embedDir topdir = do
     let pairs = makeAllRelative topdir pairs'
     embedPairs pairs
 
-embedPairs :: [(FilePath, BSL.ByteString)] -> Q Exp
+embedPairs :: [(FilePath, LBS.ByteString)] -> Q Exp
 embedPairs pairs = do
     -- we do a hop to only embed single big bytestring.
     -- it's beneficial as lzma have more stuff to compress
@@ -163,23 +163,23 @@ embedRecursiveDir topdir = do
 embedLazyByteString :: FilePath -> Q Exp
 embedLazyByteString fp = do
     qAddDependentFile fp
-    bsl <- runIO $ BSL.readFile fp
+    bsl <- runIO $ LBS.readFile fp
     lazyBytestringE bsl
 
 -- | Embed a strict 'Data.ByteString.ByteString' from a file.
 embedByteString :: FilePath -> Q Exp
-embedByteString fp = [| BSL.toStrict $(embedLazyByteString fp) :: BS.ByteString |]
+embedByteString fp = [| LBS.toStrict $(embedLazyByteString fp) :: BS.ByteString |]
 
 -- | Embed a lazy 'Data.Text.Lazy.Text' from a UTF8-encoded file.
 embedLazyText :: FilePath -> Q Exp
 embedLazyText fp = do
     qAddDependentFile fp
-    bsl <- runIO $ BSL.readFile fp
-    case TLE.decodeUtf8' bsl of
+    bsl <- runIO $ LBS.readFile fp
+    case LTE.decodeUtf8' bsl of
         Left e  -> reportError (show e)
         Right _ -> return ()
-    [| TLE.decodeUtf8 $ $(lazyBytestringE bsl) :: TL.Text |]
+    [| LTE.decodeUtf8 $(lazyBytestringE bsl) :: LT.Text |]
 
 -- | Embed a strict 'Data.Text.Text' from a UTF8-encoded file.
 embedText :: FilePath -> Q Exp
-embedText fp = [| TL.toStrict $(embedLazyText fp) :: T.Text |]
+embedText fp = [| LT.toStrict $(embedLazyText fp) :: T.Text |]
